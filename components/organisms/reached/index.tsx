25c9be35f16f3f7229bf1251d04f092cd678e894
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2022-03-01 17:47:59 +0700

    Refactor Reached

---
+++ components/organisms/reached/index.tsx
@@ -0,0 +1,20 @@
+import ReachedItem from "../../molecules/ReachedItem";
+import Line from "../../molecules/ReachedItem/line";
+
+export default function Reached() {
+  return (
+    <section className="reached pt-50 pb-50">
+      <div className="container-fluid">
+        <div className="d-flex flex-lg-row flex-column align-items-center justify-content-center gap-lg-0 gap-4">
+          <ReachedItem title=" 290M+" desc=" Players Top Up" />
+          <Line />
+          <ReachedItem title="12.500" desc=" Games Available" />
+          <Line />
+          <ReachedItem title="99,9%" desc="Happy Players" />
+          <Line />
+          <ReachedItem title="4.7" desc="Rating Worldwide" />
+        </div>
+      </div>
+    </section>
+  );
+}
